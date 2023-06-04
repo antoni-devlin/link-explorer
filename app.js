8f6e80b12a805c70f8b25ff5631c6fd0055325c9
antoni-devlin <antoni.devlin@gmail.com>
2023-06-04 11:15:54 +0100

    Added getRandomLink function to select a random link from the current page to continue navigation on

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 import * as cheerio from "cheerio";
 
+function getRandomLink(array) {
+  const filteredArray = array.filter(checkLinks);
+
+  function checkLinks(link) {
+    return !(
+      link.includes(".atom") ||
+      link.includes(".pdf") ||
+      link.includes(":") ||
+      link.includes("admin") ||
+      link.includes("/t/")
+    );
+  }
+  const randomLinkIndex = Math.floor(Math.random() * filteredArray.length);
+  let randomLink = filteredArray[randomLinkIndex];
+  if (!randomLink.includes(url) && !randomLink.includes("http")) {
+    randomLink = url + randomLink;
+  }
+  if (isValidHttpUrl(randomLink)) {
+    return randomLink;
+  } else {
+    getRandomLink(array);
+  }
+}
+
 async function findLinks(url) {
   try {
     // Fetch data from URL and store the response into a const
