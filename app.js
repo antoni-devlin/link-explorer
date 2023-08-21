c1e4077870ef70ce341d9bcdc92e2dff4f7f26cc
Antoni <antoni-devlin@users.noreply.github.com>
2023-08-21 15:04:38 +0000

    wip

--- app.js
+++ app.js
@@ -1,74 +1,69 @@
 import * as cheerio from "cheerio";
 
-function getRandomLink(array) {
-  const randomLinkIndex = Math.floor(Math.random() * array.length);
-  return array[randomLinkIndex].toString();
-}
-
-async function findLinks(url) {
+async function fetchSitemap() {
   try {
-    // Fetch data from URL and store the response into a const
-    const response = await fetch(url);
-    // Convert the response into text
+    const response = await fetch(
+      "https://alphagov.github.io/siteimprove-crawl/non-publisher-mainstream.htm"
+    );
     const html = await response.text();
 
     const $ = cheerio.load(html);
 
     const links = $("a").toArray();
 
-    const linkArray = links
-      .map((value) => {
-        return value.attribs["href"];
-      })
-      .filter((value) => value !== undefined)
-      .map((value) => {
-        return new URL(value, url);
-      })
-      .filter((link) => {
-        if (process.env.SKIP_CURRENT_PAGE) {
-          return link.toString() !== url.toString();
-        }
-        return true;
-      })
-      .filter((link) => link.protocol === "http:" || link.protocol === "https:")
-      .filter((link) => {
-        return !(
-          link.pathname.endsWith(".atom") ||
-          link.pathname.endsWith(".pdf") ||
-          link.pathname.includes("admin") ||
-          link.pathname.includes("/t/")
-        );
-      })
-      .filter((link) => link.hostname !== new URL(url).hostname)
-      .filter((link) => !link.hostname.includes("reddit"));
+    const linkArray = links.map((value) => {
+      return value.attribs["href"];
+    });
 
     return linkArray;
   } catch (error) {
     console.log(error);
-    return [];
   }
 }
 
-const url = "https://www.reddit.com";
-const baseHostname = new URL(url).hostname;
+// async function search(searchTerm) {
+//   let links = await fetchSitemap();
+//   for (const link of links) {
+//     try {
+//       const response = await fetch(link);
+//       const html = await response.text();
 
-async function navigate(nextUrl) {
-  try {
-    const links = await findLinks(nextUrl);
-    if (links.length === 0) {
-      console.log("We've hit a dead end");
-      process.exit();
+//       const $ = cheerio.load(html);
+//       const search = $(`body:contains("${searchTerm}")`).text();
+//       if (search) {
+//         console.log(`Found "${searchTerm}" on ${link}`);
+//       } else {
+//         console.log(".");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+
+let listOfUrls = await fetchSitemap();
+
+await (async (listOfUrls, MAX_PARALLEL_REQUESTS) => {
+  let requestQ = [];
+
+  for (let i = 0; i < listOfUrls.length; i++) {
+    if (requestQ.length >= MAX_PARALLEL_REQUESTS) {
+      let nextRequest = requestQ.shift();
+
+      await nextRequest.then((response) => {
+        console.log(response.text());
+      });
     }
-    const next = getRandomLink(links);
-    if (!nextUrl.includes(url)) {
-      console.log("I've escaped!");
-    }
-    console.log(`${nextUrl}`);
-    navigate(next);
-  } catch (error) {
-    console.log(`${nextUrl}`);
-    console.log(error);
+
+    requestQ.push(fetch(listOfUrls[i]));
   }
-}
 
-navigate(url);
+  while ((nextRequest = requestQ.shift())) {
+    await nextRequest.then((response) => {
+      console.log(response.text());
+    });
+  }
+})(listOfUrls, 5);
+
+fetchSitemap();
+// search("public health");
